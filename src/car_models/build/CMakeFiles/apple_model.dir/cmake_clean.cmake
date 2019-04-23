152af0a83dea4a6a672d12dd4d9fcd8b4baf4306
adarshmodh <adarshmodh>
2019-04-22 23:20:20 -0400

    carmodel

---
+++ src/car_models/build/CMakeFiles/apple_model.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apple_model.dir/apple/AppleModel.cc.o"
+  "libapple_model.pdb"
+  "libapple_model.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apple_model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/car_models/build/CMakeFiles/apple_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apple_world.dir/apple/AppleWorld.cc.o"
+  "libapple_world.pdb"
+  "libapple_world.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apple_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
