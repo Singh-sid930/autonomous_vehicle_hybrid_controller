98a59d7de0a514212385bf0b9b128dc3d95efb8d
Siddharth <singh.sid930@gmail.com>
2019-04-22 20:04:50 -0400

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/siddharth/hybrid_control/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/siddharth/hybrid_control/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/siddharth/hybrid_control/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/siddharth/hybrid_control/src:$ROS_PACKAGE_PATH"
